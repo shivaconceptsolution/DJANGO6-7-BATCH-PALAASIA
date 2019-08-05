7b6c015cb47032e2d0dc695a9183eb764348c5ce
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-08-05 19:06:40 +0530

    Add files via upload

--- pimr/studentapp/views.py
+++ pimr/studentapp/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome in Student App")
+    return render(request,"studentapp/index.html")
+def about(request):
+    return render(request,"studentapp/about.html")
+def contact(request):
+    return render(request,"studentapp/contact.html")        
+
+def si(request):
+  p=12000
+  r=2.2
+  t=2
+  si=(p*r*t)/100
+  return HttpResponse("Result is "+str(si))    
 
 
